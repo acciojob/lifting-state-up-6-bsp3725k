168d0a2a1fba852573275eaee2392d0fb0b4f4f7
bsp3725k <bhavanisivaprasad39@gmail.com>
2025-01-20 19:39:22 +0530

    Added TodoList component and updated App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-
-import React from "react";
+import React, { useState } from "react"; // Added useState to imports
 import './../styles/App.css';
+import TodoList from './TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Learn React', completed: false },
+    { id: 2, text: 'Build a project', completed: false },
+    { id: 3, text: 'Master JavaScript', completed: false },
+  ]);
+
+  const handleComplete = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: true } : todo
+      )
+    );
+  };
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+           <h2>Child Component</h2>
+        <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
